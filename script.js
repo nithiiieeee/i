74243cb7527408febca7f4f0a68323f560aa7ae5
nithin <71541854+nithiiieeee@users.noreply.github.com>
2026-02-02 14:23:04 +0530

    Add interactive cursor and touch trail effects

--- script.js
+++ script.js
@@ -1,41 +1,60 @@
-<html>
-    <head>
-        <title>Nithin Das</title>
-    </head>
+const orb = document.getElementById("orb");
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+window.addEventListener("mousemove", e => {
+  const x = (e.clientX / innerWidth - 0.5) * 30;
+  const y = (e.clientY / innerHeight - 0.5) * 30;
+  orb.style.transform = `translate(${x}px, ${y}px)`;
+});
 
-    <link href="styles.css" rel="stylesheet">
-    <body>
-        <h2 class="heading">&nbsp;Nithin das</h2>
-        <p><u>Personal Portfolio</u></p>
-        <hr>
-        <p>Table of contents</p>
-        <a href="#about-me">about me</a>
-        <br>
-        <a href="#works">my works</a>
-        <br><br>
-        <hr>
-        <div id="about-me">
-            <h3 class="heading">&nbsp;About Me</h3>
-            <p>well,<span class="select"> i am nithin</span>. someone who tries/does new stuffs when bored.i am<span class="select"> kinda at the perfect age</span> wanting to increase the quality of present and future. not too early, not too late.<span class="select"> right now</span>. and here, i am going to be blabbering about myself. i am a <a href="https://en.wikipedia.org/wiki/Chartered_accountant_(India)">CA</a> <a href="https://en.wikipedia.org/wiki/CA_Foundation_Course">foundation</a> <span class="select">student</span> with a little bit of curiosity and enthusiasm. i watch anime, movies, series, i code, i play chess, i capture, edit, i like the wind, the sunlight (not the 2pm one), the ocean, the rain, and some music. i also like to read story books from time to time. my <span class="select">aim</span> is<span class="select"> to be strong </span>and smart so that wherever you go, you can be in control if you want/need to. and so that you can <span class="select">protect your loved ones</span> ;). and i love enjoying the beauty of this nature and beings. </p>
-        </div>
-        <br><br>
-        <hr>
-        <div id="works">
-            <h3 class="heading">&nbsp;My Works</h3>
-            <p>so as i said, here are some of the things i made when i was bored.</p>
-            <p class="disclaimer">Disclaimer: some websites were made with the help of ai (due to my lack of skill). so don't judge my skill(s) based on these</p>
-        </div>
-        
-        <br><br>
-        <div class="footer">
-            <p> that's it :) &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://instagram.com/nithiiieeee">insta</a>&nbsp;&nbsp;<a href="https://github.com/nithiiieeee">github</a></p>
-        </div>
+// Cursor / touch trail
+const trailCount = 12;
+const trails = [];
 
-        <script src="script.js"></script>
+for (let i = 0; i < trailCount; i++) {
+  const t = document.createElement("div");
+  t.className = "trail";
+  document.body.appendChild(t);
+  trails.push({ el: t, x: window.innerWidth / 2, y: window.innerHeight / 2 });
+}
 
-    </body>
-</html>
+let pointer = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+let lastTouch = null;
+
+function updateTrail() {
+  let x = lastTouch ? lastTouch.x : pointer.x;
+  let y = lastTouch ? lastTouch.y : pointer.y;
+
+  trails.forEach((t, i) => {
+    // interpolate position
+    t.x += (x - t.x) * 0.25;
+    t.y += (y - t.y) * 0.25;
+    t.el.style.transform = `translate(${t.x}px, ${t.y}px)`;
+    // fade effect along the trail
+    t.el.style.opacity = (trailCount - i) / trailCount;
+    x = t.x;
+    y = t.y;
+  });
+
+  requestAnimationFrame(updateTrail);
+}
+updateTrail();
+
+// Desktop
+window.addEventListener("mousemove", e => {
+  pointer.x = e.clientX;
+  pointer.y = e.clientY;
+});
+
+// Mobile
+window.addEventListener("touchstart", e => {
+  lastTouch = { x: e.touches[0].clientX, y: e.touches[0].clientY };
+}, { passive: false });
+
+window.addEventListener("touchmove", e => {
+  e.preventDefault(); // prevent scrolling while dragging
+  lastTouch = { x: e.touches[0].clientX, y: e.touches[0].clientY };
+}, { passive: false });
+
+window.addEventListener("touchend", () => {
+  lastTouch = null;
+});
